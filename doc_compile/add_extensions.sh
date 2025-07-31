4686c149f03647dec0ab964321050e5bc88170ee
WillForan <willforan@gmail.com>
2025-07-31 14:25:37 -0400

    🚧 doc: sphinix init

--- doc_compile/add_extensions.sh
+++ doc_compile/add_extensions.sh
@@ -5,9 +5,10 @@
 # 20250731WF - init
 #
 ! test -r ../mkifdiff  && echo "ERROR: no $_! run from wrong dir?" && exit 1
-mkdir -p src
-find -maxdepth 1 -type f | xargs grep '^#!' -m1 |
-   perl -lne '
+export OUTPUT=$PWD/src
+mkdir -p $OUTPUT
+find .. -maxdepth 1 -type f | xargs grep '^#!' -m1 |
+   perl -mFile::Basename=basename -lne '
   BEGIN{ %ext=qw(sh sh bash bash 
                  perl pl 
                  octave m matlab m 
@@ -16,7 +17,7 @@ find -maxdepth 1 -type f | xargs grep '^#!' -m1 |
                  Rscript R);
  }
  $keys=join("|", keys %ext);
- print if s;(.+):#!.*($keys).*;"${1} src/${1}.".$ext{$2};ei'  |
+ print if s;(.+):#!.*($keys).*;"${1} $ENV{OUTPUT}/".basename(${1}).".".$ext{$2};ei'  |
 while read bin w_ext; do
-   cat "$bin" | dryrun ../mkifdiff "$w_ext"
+   cat "$bin" | dryrun mkifdiff "$w_ext"
 done
